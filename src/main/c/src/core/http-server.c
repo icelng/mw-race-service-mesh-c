d49aea55eba44c89c7a18f2646e844e1eb417097
yiran_fancier <ip28@qq.com>
2018-05-28 15:15:10 +0800

    修改close线程请求队列

--- src/main/c/src/core/http-server.c
+++ src/main/c/src/core/http-server.c
@@ -38,7 +38,7 @@ struct hs_handle* hs_start(struct hs_bootstrap *hs_bt){
 
     /*创建线程池内存池*/
     log_info("Creating worker thread pool.");
-    p_new_hs_handle->tdpl_worker = tdpl_create(hs_bt->worker_thread_num, 51200);  // n个线程，51200个等待
+    p_new_hs_handle->tdpl_worker = tdpl_create(hs_bt->worker_thread_num, 512);  // n个线程，51200个等待
     if (p_new_hs_handle->tdpl_worker == NULL) {
         log_err("启动http-server时，worker线程池创建失败!%s", strerror(errno));
         goto err2_ret;
@@ -52,7 +52,7 @@ struct hs_handle* hs_start(struct hs_bootstrap *hs_bt){
     }
 
     log_info("Creating close thread pool.");
-    p_new_hs_handle->tdpl_close = tdpl_create(8, 5120);  // 8个线程负责关闭，51200个等待
+    p_new_hs_handle->tdpl_close = tdpl_create(8, 512);  // 8个线程负责关闭，51200个等待
     if (p_new_hs_handle->tdpl_close == NULL) {
         log_err("启动http-server时，线程池close创建失败!%s", strerror(errno));
         goto err2_ret;
