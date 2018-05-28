0eab28101205111272a8f0c1259d98b6466a1766
yiran_fancier <ip28@qq.com>
2018-05-28 14:30:55 +0800

    修改DockerFile

--- start-agent.sh
+++ start-agent.sh
@@ -8,7 +8,7 @@ echo ETCD_URL = $ETCD_URL
 
 if [[ "$1" == "consumer" ]]; then
   echo "Starting consumer agent..."
-  #/root/dists/consumer-agent
+  /root/dists/consumer-agent
 elif [[ "$1" == "provider-small" ]]; then
   echo "Starting small provider agent..."
   java -jar \
